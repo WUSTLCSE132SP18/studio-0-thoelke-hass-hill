dfb5f60c4d78afad713e0e7bc4ce01f6557f1eaf
jessicathoelke <jessicathoelke@jessicas-mbp-2.dhcp.wustl.edu>
2018-01-17 15:27:48 -0600

    studio 0

---
+++ heartbeat/heartbeat.ino
@@ -0,0 +1,17 @@
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+pinMode(13, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+delay(1000);
+digitalWrite(13, HIGH);
+delay(1000);
+digitalWrite(13, LOW);
+int time = millis();
+Serial.print(time);
+Serial.println("seconds have elapsed");
+
+}
